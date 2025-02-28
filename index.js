7af0b3e9f03b7c728134df57399f193ebd1d52fe
AilinMari <ailin_scilla@hotmail.com>
2025-02-28 13:10:36 +0100

    links

--- index.js
+++ index.js
@@ -2,25 +2,22 @@
 let currentIndex = 0;
 
 function changeElement(direction) {
-    const elements = document.querySelectorAll(".logo-letters h2");
-    const totalElements = elements.length;
-    elements[currentIndex].classList.remove("visible");
-    currentIndex = (currentIndex + direction + totalElements) % totalElements;
-    elements[currentIndex].classList.add("visible");
+  const elements = document.querySelectorAll(".logo-letters h2");
+  const totalElements = elements.length;
+  elements[currentIndex].classList.remove("visible");
+  currentIndex = (currentIndex + direction + totalElements) % totalElements;
+  elements[currentIndex].classList.add("visible");
 }
 
 // Initialize the first element
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll(".logo-letters h2").forEach((element, index) => {
-        element.classList.add("fade");
-        if (index === 0) {
-            element.classList.add("visible");
-        }
-    });
+  document.querySelectorAll(".logo-letters h2").forEach((element, index) => {
+    element.classList.add("fade");
+    if (index === 0) {
+      element.classList.add("visible");
+    }
+  });
 
-    // Change element every 2 seconds automatically
-    setInterval(() => changeElement(1), 2000);
+  // Change element every 2 seconds automatically
+  setInterval(() => changeElement(1), 2000);
 });
-
-
-
